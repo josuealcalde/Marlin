e18cfd9791754c5b9133c8c0e47b7745f7c0b23e
Josué Alcalde González <josue.alcalde@medac.es>
2024-01-28 21:05:44 +0100

    Permitir toda la superficie de la cama.
    Cambiar tb valores de direct drive y poner el zoffset en -3.10

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1484,7 +1484,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -35, -45, -2.95 }
+#define NOZZLE_TO_PROBE_OFFSET { -35, -45, -3.10 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
